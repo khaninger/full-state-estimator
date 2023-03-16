eeed0b9c4f163572239b9914f38437f6be15a1dc
Roberta <roberta@MacBook-Air-di-Roberta.local>
2023-03-16 11:52:38 +0100

    local merge

--- Hybrid_filter.py
+++ Hybrid_filter.py
@@ -4,6 +4,8 @@ import numpy as np
 import copy
 from new_params import *
 from scipy.stats import multivariate_normal
+from observer import ekf
+from robot import robot
 
 class HybridParticleFilter:
     def __init__(self, mu_0, mode_0, )
@@ -24,3 +26,77 @@ class HybridParticleFilter:
         self.ekf = ekf(self.params,
                             np.array([2.29, -1.02, -0.9, -2.87, 1.55, 0.56]),
                             est_geom, est_stiff)
+        for i in range(self.num_particles):
+            self.particles.append(Particle(mode_0, mu_0, P0, 1/N, y_hat0, S_t0, np.random.choice(['free-space', 'contact'], p=mode_0)))
+    def step(self):
+        for particle in self.particles:
+            particle.mode = np.matmul(particle.mode_prev, self.trans_matrix)
+            particle.sampled_mode = np.random.choice(['free-space', 'contact'], p=particle.mode)
+            if particle.sampled_mode == 'free-space':
+                particle.mu = ekf.step(          dyn_sys=robot(par))[0]
+                particle.Sigma = ekf.step(          dyn_sys=robot(par))[2]
+                particle.S_t = ekf.step(          dyn_sys=robot(par))[4]
+                particle.y_hat = ekf.step(          dyn_sys=robot(par))[5]
+            elif particle.sampled_mode == 'contact':
+                particle.mu = ekf.step(dyn_sys=robot(par, est_par = est_par))[0]
+                particle.Sigma = ekf.step(dyn_sys=robot(par, est_par = est_par))[2]
+                particle.S_t = ekf.step(dyn_sys=robot(par, est_par = est_par))[4]
+                particle.y_hat = ekf.step(dyn_sys=robot(par, est_par = est_par))[5]
+
+    def calc_weights(self, obs):
+        summation = 0
+        for particle in self.particles:
+            particle.weight = multivariate_normal(mean=particle.y_hat, cov=particle.S_t).pdf(obs)
+            if particle.weight<1e-15:
+                particle.weight = sys.float_info.epsilon
+            summation += particle.weight
+        self.weightsum = 0
+        for particle in self.particles:
+            particle.weight /= summation
+            if particle.weight == 0:
+                particle.weight = sys.float_info.epsilon
+            self.weightsum += particle.weight
+        self.belief_free = sum([particle.weight for particle in self.particles if particle.sampled_mode == 'free-space'])
+        self.belief_contact = sum([particle.weight for particle in self.particles if particle.sampled_mode == 'contact'])
+        for particle in self.particles:
+            particle.mode_prev = np.array([self.belief_free, self.belief_contact])
+
+    def estimate_state(self):
+        pos = np.zeros(self.num_particles)
+        weights = np.zeros(self.num_particles)
+        vel = np.zeros(self.num_particles)
+        x_env = np.zeros(self.num_particles)
+        for i, particle in enumerate(self.particles):
+            pos[i] = particle.mu[0]
+            weights[i] = particle.weight
+            vel[i] = particle.mu[1]
+        self.x_hat = np.average(pos, weights=weights, axis=0)
+        self.x_dot_hat = np.average(vel, weights=weights, axis=0)
+    def MultinomialResample(self):
+        states_idx = []
+        weights = []
+        states = []
+        for i in range(len(self.particles)):
+            states_idx.append([self.particles[i].weight, i])
+            weights.append(self.particles[i].weight)
+
+        resamp_parts = []
+
+        temp = np.random.multinomial(len(self.particles), weights)
+        for i in range(len(temp)):
+            for j in range(temp[i]):
+                resamp_parts.append(copy.deepcopy(self.particles[i]))
+
+        self.particles = resamp_parts
+
+    def RunFilter(self, z):
+        self.PropagateState()
+        self.CalcWeights(z)
+        self.MultinomialResample()
+        self.EstimateState()
+        return self.x_hat, self.x_dot_hat, self.belief_free, self.belief_contact
+class Particle:
+    def __init__(self):
+
+
+
--- observer.py
+++ observer.py
@@ -1,6 +1,7 @@
 import casadi as ca
 import numpy as np
 from robot import robot
+from scipy.stats import multivariate_normal
 
 class ekf():
     """ This defines an EKF observer """
@@ -41,7 +42,7 @@ class ekf():
                                      self.x.get('p', []),
                                      self.x.get('stiff', []))}
         #print(self.cov)
-        x_next = dyn_sys.disc_dyn.call(step_args)  # predict state and output at next time step
+        self.x_next = dyn_sys.disc_dyn.call(step_args)  # predict state and output at next time step
         A, C = dyn_sys.get_linearized(step_args)   # get the linearized dynamics and observation matrices
         #print(f"F_i = {self.dyn_sys.jacpinv(self.x['q']).T@x_next['tau_err']}")
         #print(f"tau_err = {x_next['tau_err']}")
@@ -49,35 +50,35 @@ class ekf():
         #print(step_args['tau_err'])
         #print(q-x_next['xi_next'][:6])
         #print(A)
-        cov_next = A@self.cov@(A.T) + self.proc_noise
+        self.cov_next = A@self.cov@(A.T) + self.proc_noise
         self.S = C@self.cov_next@(C.T) + self.meas_noise
-        self.y_hat = C@x_next
+        self.y_hat = C@self.x_next
         #print(cov_next)
-        self.L = cov_next@C.T@ca.inv(C@cov_next@(C.T) + self.meas_noise) # calculate Kalman gain
+        self.L = self.cov_next@C.T@ca.inv(C@self.cov_next@(C.T) + self.meas_noise) # calculate Kalman gain
         if np.any(np.isnan(self.L)): raise ValueError("Nans in the L matrix")
     
-        xi_corr = x_next['xi_next'] + self.L@(q - x_next['xi_next'][:self.dyn_sys.nq])
+        xi_corr = self.x_next['xi_next'] + self.L@(q - self.x_next['xi_next'][:self.dyn_sys.nq])
         #print((L@(q - x_next['xi_next'][:self.dyn_sys.nq]))[-3:])
         #print(xi_corr)
         self.x['q'] = xi_corr[:self.dyn_sys.nq].full()
         self.x['dq'] = xi_corr[self.dyn_sys.nq:2*self.dyn_sys.nq].full()
         if self.est_geom: self.x['p'] = xi_corr[2*self.dyn_sys.nq:].full()
         if self.est_stiff: self.x['stiff'] = xi_corr[2*self.dyn_sys.nq:].full().flatten()
-        self.x['cont_pt'] = x_next['cont_pt'].full().flatten()
+        self.x['cont_pt'] = self.x_next['cont_pt'].full().flatten()
         x_ee = self.dyn_sys.fwd_kin(self.x['q'])
         self.x['x_ee'] = (x_ee[0].full(),
                           x_ee[1].full())
         self.x['xi'] = xi_corr.full()
-        self.mom_obs.step(x_next['mom'], x_next['tau_err'])
+        self.mom_obs.step(self.x_next['mom'], self.x_next['tau_err'])
         self.x['f_ee_mo'] = (self.dyn_sys.jacpinv(self.x['q'])@self.mom_obs.r).full()
-        self.x['f_ee_obs'] = -(self.dyn_sys.jacpinv(self.x['q'])@x_next['tau_i']).full()
+        self.x['f_ee_obs'] = -(self.dyn_sys.jacpinv(self.x['q'])@self.x_next['tau_i']).full()
         
-        self.cov = (ca.DM.eye(self.dyn_sys.nx)-self.L@C)@cov_next # corrected covariance
+        self.cov = (ca.DM.eye(self.dyn_sys.nx)-self.L@C)@self.cov_next # corrected covariance
         #print(self.cov[-3:,-3:])
-        return self.x, self.cov, self.S, self.y_hat
+        return self.x, self.x_next, self.cov, self.cov_next, self.S, self.y_hat,
 
     def likelihood(self, obs):
-        return
+        return NotImplemented
 
 class MomentumObserver():
     ''' Mostly following https://elib.dlr.de/129060/1/root.pdf '''
