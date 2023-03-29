b01fb17ca44c86a9f34ed49e79cb2aaab5740f10
IPK410 <christian-hegelert-online.de>
2023-03-29 19:11:17 +0200

    add static ekf update

--- Hybrid_filter.py
+++ Hybrid_filter.py
@@ -2,15 +2,17 @@ import pdb
 import sys
 import numpy as np
 import copy
-
+import casadi as ca
 from scipy.stats import multivariate_normal
 from observer import ekf
 from robot import robot
-import copy
+from observer import static_ekf_update
+
 
 class HybridParticleFilter(ekf):
     def __init__(self,  par, q0, est_geom = False, est_stiff = False):
         ekf.__init__(self, par, q0, est_geom, est_stiff)
+
         self.particles = []
         self.num_particles = par['num_particles']
         self.trans_matrix = par['transition_matrix']
@@ -25,7 +27,64 @@ class HybridParticleFilter(ekf):
         for i in range(self.num_particles):
             self.particles.append(Particle(self.belief_init, self.x, self.cov, 1/self.num_particles, sampled_mode0=np.random.choice(['free-space', 'contact'], p=self.belief_init)))
 
+    def step_ekf(self, q, tau, dyn_sys, x, cov, F=None):
+        """ Steps the observer based on the input at time t and observation at time t
+            Standard EKF update, see, e.g. pg. 51 in Thrun "Probabilistic Robotics" """
+        step_args = {'tau': tau,
+                     'xi': ca.vertcat(x['q'], x['dq'],
+                                      x.get('p', []),
+                                      x.get('stiff', []))}
+        # print(self.cov)
+        x_next = dyn_sys.disc_dyn.call(step_args)  # predict state and output at next time step
+        print(x_next)
+        x_next = x_next["xi_next"]
+        # print(x_next.shape)
+        A, C = dyn_sys.get_linearized(step_args)  # get the linearized dynamics and observation matrices
+        # print(C.shape)
+        # print(f"F_i = {self.dyn_sys.jacpinv(self.x['q']).T@x_next['tau_err']}")
+        # print(f"tau_err = {x_next['tau_err']}")
+        # print(f"tau     = {tau}")
+        # print(step_args['tau_err'])
+        # print(q-x_next['xi_next'][:6])
+        # print(A)
+        # print(np.argwhere(np.isnan(self.cov)))
+        cov_next = A @ cov @ (A.T) + self.proc_noise
+        # print(C)
 
+        # print(self.cov)
+        S = C @ cov_next @ (C.T) + self.meas_noise
+        y_hat = C @ x_next
+        # print(self.y_hat.shape)
+        # print(self.cov_next)
+        L = cov_next @ C.T @ ca.inv(C @ cov_next @ (C.T) + self.meas_noise)  # calculate Kalman gain
+        # print(self.L.shape)
+        if np.any(np.isnan(L)): raise ValueError("Nans in the L matrix")
+
+        xi_corr = x_next['xi_next'] + L @ (q - x_next['xi_next'][dyn_sys.nq])
+        # print((L@(q - x_next['xi_next'][:self.dyn_sys.nq]))[-3:])
+        # print(xi_corr)
+        x['q'] = xi_corr[:dyn_sys.nq].full()
+        x['dq'] = xi_corr[dyn_sys.nq:2 * dyn_sys.nq].full()
+        if self.est_geom: x['p'] = xi_corr[2 * dyn_sys.nq:].full()
+        if self.est_stiff: x['stiff'] = xi_corr[2 * dyn_sys.nq:].full().flatten()
+        x['cont_pt'] = x_next['cont_pt'].full().flatten()
+        x_ee = dyn_sys.fwd_kin(x['q'])
+        x['x_ee'] = (x_ee[0].full(),
+                          x_ee[1].full())
+        x['xi'] = xi_corr.full()
+        mom_obs.step(x_next['mom'], x_next['tau_err'])
+        x['f_ee_mo'] = (dyn_sys.jacpinv(x['q']) @ mom_obs.r).full()
+        x['f_ee_obs'] = -(dyn_sys.jacpinv(x['q']) @ x_next['tau_i']).full()
+
+        cov = (ca.DM.eye(dyn_sys.nx) - L @ C) @ cov_next  # corrected covariance
+        # print("debug2")
+        # print(self.cov)
+        # print(self.cov[-3:,-3:])
+        # x_est = copy.deepcopy(self.x)
+        # cov_est = copy.deepcopy(self.cov)
+        # S = copy.deepcopy(self.S)
+        # y_hat = copy.deepcopy(self.y_hat)
+        return x, cov, S, y_hat
 
 
 
@@ -44,28 +103,42 @@ class HybridParticleFilter(ekf):
         for i,particle in enumerate(self.particles):
             particle.mode = np.matmul(particle.mode_prev, self.trans_matrix)
             particle.sampled_mode = np.random.choice(['free-space', 'contact'], p=particle.mode)
+            #particle.mu = copy.deepcopy(particle.mu)
+            #particle.Sigma = copy.deepcopy(particle.Sigma)
+
             if particle.sampled_mode == 'free-space':
-                particle.mu = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_free)[0]
-                particle.Sigma = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_free)[1]
+                res = self.step_ekf(q=q, tau=tau, dyn_sys=self.dyn_contact, x=self.x, cov=self.cov)
+                #particle.mu = self.step(q=q, tau=tau, dyn_sys=self.dyn_contact)[0]
+                #self.S_est[i], self.y_est[i] = particle.step(q=q, tau=tau, dyn_sys=self.dyn_free)[-2:]
+                #particle.mu, particle.Sigma, self.S_est[i], self.y_est[i] = static_ekf_update(cov=self.cov, x=self.x, q=q, tau=tau, dyn_sys=self.dyn_free, proc_noise=self.proc_noise, meas_noise=self.meas_noise, est_geom=self.est_geom, est_stiff=self.est_stiff)
+                #particle.mu, particle.Sigma, self.S_est[i], self.y_est[i] = self.step_ekf(q=q, tau=tau, dyn_sys=self.dyn_free, x=self.x, cov=self.cov)
 
-                #print(particle.Sigma)
-                self.S_est[i] = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_free)[2]
-
-                self.y_est[i] = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_free)[3]
+                #particle.Sigma = self.step(q=q, tau=tau, dyn_sys=self.dyn_free)[1]
+                #self.S_est[i] = self.step(q=q, tau=tau, dyn_sys=self.dyn_free)[2]
+                #self.y_est[i] = self.step(q=q, tau=tau, dyn_sys=self.dyn_free)[3]
                 #print(self.y_est[i].shape)
                 #print(self.S_t[i].shape)
             elif particle.sampled_mode == 'contact':
-                particle.mu = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_contact)[0]
-                particle.Sigma = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_contact)[1]
+                res = self.step_ekf(q=q, tau=tau, dyn_sys=self.dyn_contact, x=self.x, cov=self.cov)
+
+                #particle.mu, particle.Sigma, self.S_est[i], self.y_est[i] = self.step_ekf(q=q, tau=tau, dyn_sys=self.dyn_contact, x=self.x, cov=self.cov)
+
+                # if particle inherits ekf
+                #self.S_est[i], self.y_est[i] = particle.step(q=q, tau=tau, dyn_sys=self.dyn_contact)[-2:]
+
+
+                #particle.mu = self.step(q=q, tau=tau, dyn_sys=self.dyn_contact)[0]
+                #particle.Sigma = self.step(q=q, tau=tau, dyn_sys=self.dyn_contact)[1]
                 #print(particle.Sigma)
-                self.S_est[i] = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_contact)[2]
-                self.y_est[i] = ekf.step(self, q=q, tau=tau, dyn_sys=self.dyn_contact)[3]
+                #self.S_est[i] = self.step(q=q, tau=tau, dyn_sys=self.dyn_contact)[2]
+                #self.y_est[i] = self.step(q=q, tau=tau, dyn_sys=self.dyn_contact)[3]
             #print("debug1")
-            #print(self.S_est[i])
+            print(particle.Sigma)
+
+            #print(self.y_est[i].shape)
             #print(np.linalg.eigvalsh(self.S_est[i]))
             #print("debug transpose")
             #print(print(self.S_est[i].T))
-            #print(particle.Sigma)
             #print(particle.sampled_mode)
 
     def calc_weights(self, q):
@@ -92,7 +165,7 @@ class HybridParticleFilter(ekf):
         pos = np.zeros(self.num_particles)
         weights = np.zeros(self.num_particles)
         vel = np.zeros(self.num_particles)
-        x_env = np.zeros(self.num_particles)
+        #x_env = np.zeros(self.num_particles)
         for i, particle in enumerate(self.particles):
             pos[i] = particle.mu['q']
             weights[i] = particle.weight
@@ -122,8 +195,10 @@ class HybridParticleFilter(ekf):
         self.MultinomialResample()
         self.estimate_state()
         return self.x_hat, self.x_dot_hat, self.belief_free, self.belief_contact
-class Particle:
+
+class Particle(ekf):
     def __init__(self, mode_0, mu_0, P0, weight0, sampled_mode0):
+        #ekf.__init__(self, par, q0, est_geom, est_stiff)
         self.mode = self.mode_prev = mode_0
         self.mu = mu_0
         self.Sigma = P0
--- observer.py
+++ observer.py
@@ -4,6 +4,39 @@ from robot import robot
 from scipy.stats import multivariate_normal
 import copy
 
+def static_ekf_update(cov, x, q, tau, dyn_sys, proc_noise, meas_noise, est_geom, est_stiff, F=None):
+    step_args = {'tau':tau,
+                 'xi':ca.vertcat(x['q'], x['dq'],
+                                     x.get('p', []),
+                                     x.get('stiff', []))}
+    x_next = dyn_sys.disc_dyn.call(step_args)
+    #print(x_next)
+    x_next = x_next["xi_next"]
+    #print(x_next)
+    A, C = dyn_sys.get_linearized(step_args)  # get the linearized dynamics and observation matrices
+    cov_next = A @ cov @ (A.T) + proc_noise
+    S = C @ cov_next @ (C.T) + meas_noise
+    y_hat = C @ x_next
+    L = cov_next @ C.T @ ca.inv(C @ cov_next @ (C.T) + meas_noise)  # calculate Kalman gain
+    if np.any(np.isnan(L)): raise ValueError("Nans in the L matrix")
+    xi_corr = x_next['xi_next'] + L @ (q - x_next['xi_next'][dyn_sys.nq])
+    x['q'] = xi_corr[:dyn_sys.nq].full()
+    x['dq'] = xi_corr[dyn_sys.nq:2 * dyn_sys.nq].full()
+    if est_geom: x['p'] = xi_corr[2 * dyn_sys.nq:].full()
+    if est_stiff: x['stiff'] = xi_corr[2 * dyn_sys.nq:].full().flatten()
+    x['cont_pt'] = x_next['cont_pt'].full().flatten()
+    x_ee = dyn_sys.fwd_kin(x['q'])
+    x['x_ee'] = (x_ee[0].full(),
+                      x_ee[1].full())
+    x['xi'] = xi_corr.full()
+    mom_obs.step(x_next['mom'], x_next['tau_err'])
+    x['f_ee_mo'] = (dyn_sys.jacpinv(x['q']) @ mom_obs.r).full()
+    x['f_ee_obs'] = -(dyn_sys.jacpinv(x['q']) @ x_next['tau_i']).full()
+
+    cov = (ca.DM.eye(dyn_sys.nx) - L @ C) @ cov_next  # corrected covariance
+
+    return x, cov, S, y_hat
+
 class ekf():
     """ This defines an EKF observer """
     def __init__(self, par, q0, est_geom = False, est_stiff = False):
@@ -34,9 +67,11 @@ class ekf():
         
         self.meas_noise = np.diag(par['meas_noise']['pos'])
         self.mom_obs = MomentumObserver(par, q0)
+
+
         
     def step(self, q, tau, dyn_sys, F = None):
-        """ Steps the observer baed on the input at time t and observation at time t
+        """ Steps the observer based on the input at time t and observation at time t
             Standard EKF update, see, e.g. pg. 51 in Thrun "Probabilistic Robotics" """
         step_args = {'tau':tau,
                      'xi':ca.vertcat(self.x['q'], self.x['dq'],
@@ -56,15 +91,15 @@ class ekf():
         #print(q-x_next['xi_next'][:6])
         #print(A)
         #print(np.argwhere(np.isnan(self.cov)))
-        cov_next = A@self.cov@(A.T) + self.proc_noise
+        self.cov_next = A@self.cov@(A.T) + self.proc_noise
         #print(C)
 
         #print(self.cov)
-        self.S = C@cov_next@(C.T) + self.meas_noise
+        self.S = C@self.cov_next@(C.T) + self.meas_noise
         self.y_hat = C@x_next
         #print(self.y_hat.shape)
-        #print(cov_next)
-        self.L = cov_next@C.T@ca.inv(C@cov_next@(C.T) + self.meas_noise) # calculate Kalman gain
+        #print(self.cov_next)
+        self.L = self.cov_next@C.T@ca.inv(C@self.cov_next@(C.T) + self.meas_noise) # calculate Kalman gain
         #print(self.L.shape)
         if np.any(np.isnan(self.L)): raise ValueError("Nans in the L matrix")
     
@@ -84,11 +119,15 @@ class ekf():
         self.x['f_ee_mo'] = (dyn_sys.jacpinv(self.x['q'])@self.mom_obs.r).full()
         self.x['f_ee_obs'] = -(dyn_sys.jacpinv(self.x['q'])@self.x_next['tau_i']).full()
         
-        self.cov = (ca.DM.eye(dyn_sys.nx)-self.L@C)@cov_next  # corrected covariance
+        self.cov = (ca.DM.eye(dyn_sys.nx)-self.L@C)@self.cov_next  # corrected covariance
         #print("debug2")
-        print(self.cov)
+        #print(self.cov)
         #print(self.cov[-3:,-3:])
-        return self.x, self.cov, self.S, self.y_hat,
+        #x_est = copy.deepcopy(self.x)
+        #cov_est = copy.deepcopy(self.cov)
+        #S = copy.deepcopy(self.S)
+        #y_hat = copy.deepcopy(self.y_hat)
+        return self.x, self.cov, self.S, self.y_hat
 
     def likelihood(self, obs):
         return NotImplemented
