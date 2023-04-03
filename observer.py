07b4436855cbad7f4d885dd51d8e49a664a266ad
Kevin Haninger <kevin@haninger>
2023-04-03 14:31:38 +0200

    move build_step_fn to static

--- observer.py
+++ observer.py
@@ -2,37 +2,37 @@ import casadi as ca
 import numpy as np
 from robot import Robot
 
+def build_step_fn(self, robot):
+    # Build a static KF update w arguments of mu, sigma, tau and measured q
+    proc_noise = robot.proc_noise
+    meas_noise = robot.meas_noise
+
+    tau = robot.vars['tau']
+    mu = robot.vars['xi']
+    mu_next = robot.vars['xi_next']
+    A, C = robot.get_linearized_opt({'tau':tau, 'xi':mu})
+
+    q_meas = ca.SX.sym('q_meas', robot.nq)
+    cov = ca.SX.sym('cov', mu.shape[0], mu.shape[0])
+
+    cov_next = A@cov@(A.T) + proc_noise
+    L = cov_next@C.T@ca.inv(C@cov_next@(C.T) + meas_noise) # calculate Kalman gain
+
+    mu_next_corr = mu_next + L@(q_meas - mu_next[:robot.nq])
+    cov_next_corr = (ca.SX.eye(robot.nx)-L@C)@cov_next # corrected covariance
+
+    fn_dict = {'tau':tau, 'mu':mu, 'cov':cov, 'q_meas':q_meas,
+               'mu_next':mu_next_corr, 'cov_next':cov_next_corr}
+    self.step_fn = ca.Function('ekf_step', fn_dict,
+                               ['tau', 'mu', 'cov', 'q_meas'], # inputs to casadi function
+                               ['mu_next', 'cov_next'])        # outputs of casadi function
+    return step_fn
+
 class ekf():
     """ This defines an EKF observer """
     def __init__(self, robot):
-        self.x = {'mu':robot.xi_init, 'cov':robot.cov_init}
-        self.proc_noise = robot.proc_noise
-        self.meas_noise = robot.meas_noise
-        
-        self.build_step_fn(robot)
-
-    def build_step_fn(self, robot):
-        # Build a static KF update w arguments of mu, sigma, tau and measured q
-        tau = robot.vars['tau']
-        mu = robot.vars['xi']
-        mu_next = robot.vars['xi_next']
-        A, C = robot.get_linearized_opt({'tau':tau, 'xi':mu})
-        
-        q_meas = ca.SX.sym('q_meas', robot.nq)
-        cov = ca.SX.sym('cov', mu.shape[0], mu.shape[0])
-        
-        cov_next = A@cov@(A.T) + self.proc_noise
-        L = cov_next@C.T@ca.inv(C@cov_next@(C.T) + self.meas_noise) # calculate Kalman gain
-
-        mu_next_corr = mu_next + L@(q_meas - mu_next[:robot.nq])
-        cov_next_corr = (ca.SX.eye(robot.nx)-L@C)@cov_next # corrected covariance
-
-        fn_dict = {'tau':tau, 'mu':mu, 'cov':cov, 'q_meas':q_meas,
-                   'mu_next':mu_next_corr, 'cov_next':cov_next_corr}
-        self.step_fn = ca.Function('ekf_step', fn_dict,
-                                   ['tau', 'mu', 'cov', 'q_meas'], # inputs to casadi function
-                                   ['mu_next', 'cov_next'])        # outputs of casadi function
-        return self.step_fn
+        self.x = {'mu':robot.xi_init, 'cov':robot.cov_init} 
+        self.step_fn = build_step_fn(robot)
 
     
     def step_fast(self, q, tau, F=None):
